8f3f020774c76953abc7c23d88308daef1acc39b
Nahuel Rodriguez <nahufed@gmail.com>
2025-02-25 12:53:22 -0300

    added auth endpoints

---
+++ src/constants/endpoints-API.ts
@@ -0,0 +1,11 @@
+export const URI_LOGIN = 'http://localhost:3000/api/v1/auth/login';
+
+export const URI_REGISTER = 'http://localhost:3000/api/v1/auth/register';
+
+export const URI_FORGOT_PASSWORD = 'http://localhost:3000/api/v1/auth/forgot-password'
+
+export const URI_CHANGE_PASSWORD = 'http://localhost:3000/api/v1/auth/change-password'
+
+export const URI_CONFIRM_PASSWORD = 'http://localhost:3000/api/v1/auth/confirm-password'
+
+
